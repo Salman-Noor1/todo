f84d7a50261e666244157aef856c066b238102f4
[Salman-Noor1] <[salmannoormohammed769@gmail.com]>
2022-09-15 01:08:55 +0300

    hello

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
+from unicodedata import name
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    name = "Salman Noor"
+
+    context = {
+        "name":name
+    }
+    return render(request, "base.html", context)
